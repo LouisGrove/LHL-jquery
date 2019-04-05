b95da9e35c041eea29c6229879c533f58b5ab3aa
Louis Grove <48039028+LouisGrove@users.noreply.github.com>
2019-04-04 20:05:33 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,40 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <!-- Include the stylesheet -->
-    <link rel="stylesheet" href="./styles.css">
-    <title>jQuery Horoscope Project</title>
-  </head>
-  <body>
-    <header>
-      <h1>My jQuery Horoscope Project</h1>
-      <button type="submit" id="searchAgainButton">Search Again</button>
-    </header>
-    <main>
-      <form>
-        <input type="text" id="Horoscope-Value" name="search-term" />
-        <button type="submit">Search</button>
-      </form>
-      <div id="results">
-      </div>
-    </main>
-    <footer>
-      <!-- Add footer content -->
-    </footer>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script type="text/javascript" src="./index.js"></script>
-  </body>
-</html>
+$(document).ready(function() {
+
+  function appendHoroscopeData(obj) {
+    $('#results').append("<h2>" + obj.sign + "</h2>");
+    $('#results').append("<p>" + obj.date + "</p>");
+    $('#results').append("<p>" + obj.horoscope + "</p>");
+  }
+
+  $('form').on('submit', function(event) {
+    event.preventDefault();
+    $("#results").children().remove();
+    $('form').hide(500);
+    $("#searchAgainButton").show(500);
+    var horoscopeValue = $("#Horoscope-Value").val()
+    console.log(horoscopeValue)
+
+
+
+
+  $.get('http://horoscope-lhl.herokuapp.com/horoscopes/'+horoscopeValue, function(data) {
+      console.log(data)
+      appendHoroscopeData(data)
+    }
+)
+
+
+
+
+  })
+
+  $('#searchAgainButton').on('click', function(event) {
+    event.preventDefault();
+    console.log("clicked")
+    $("#Horoscope-Value").val("")
+    $('form').show(500);
+    $("#results").children().remove();
+    $("#searchAgainButton").hide(500);
+
+  })
+});
