c82d736b6360442234da33f0ef72623a6ef07d95
Louis Grove <48039028+LouisGrove@users.noreply.github.com>
2019-04-04 19:18:11 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <!-- Include the stylesheet -->
+    <link rel="stylesheet" href="./styles.css">
+    <title>jQuery Horoscope Project</title>
+  </head>
+  <body>
+    <header>
+      <h1>My jQuery Horoscope Project</h1>
+      <button type="submit" id="searchAgainButton">Search Again</button>
+    </header>
+    <main>
+      <form>
+        <input type="text" id="Horoscope-Value" name="search-term" />
+        <button type="submit">Search</button>
+      </form>
+      <div id="results">
+      </div>
+    </main>
+    <footer>
+      <!-- Add footer content -->
+    </footer>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script type="text/javascript" src="./index.js"></script>
+  </body>
+</html>
